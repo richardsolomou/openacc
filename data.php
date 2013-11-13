76f4dac08dae694467f7bd80574d6c9a6cec75b1
Richard Solomou <rsolomou@gmail.com>
2013-11-13 01:09:25 +0000

    Added commenting to the data file.

--- data.php
+++ data.php
@@ -1,1 +1,4 @@
-<?php echo file_get_contents('http://openacc-web-01.uni.ds.port.ac.uk/dsign/oainfo.php'); ?>
+<?php
+	// Returns the contents of the JSON encoded array with the data.
+	echo file_get_contents('http://openacc-web-01.uni.ds.port.ac.uk/dsign/oainfo.php');
+?>
