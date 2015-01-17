438ee41b0c0442c0a028c5391dcf3afc0fc80cf7
Richard Solomou <rsolomou@gmail.com>
2015-01-17 14:51:40 +0000

    Updated to show closest building.

--- js/scripts.js
+++ js/scripts.js
@@ -3,94 +3,106 @@ $(document).ready(function() {
 
 	// Main function to run.
 	var main = function(lat, lon, acc) {
-		// Set the access token for the API.
-		var api = 'http://ssd.api.port.ac.uk/v1/',
-			access_token = '?access_token=bd368b12-4aed-4f5e-8797-3aa9cfe21395&callback=?';
+		// Prints out the latitude, longitude and accuracy in the console.
+		console.log('Latitude: ' + lat + '\r\n' + 'Longitude: ' + lon + '\r\n' + 'Accuracy: ' + acc);
 		// Loads the JSON-encoded data from the server using a GET HTTP request.
-		$.getJSON(api + 'buildings/openaccess' + access_token, function (openaccess) {
-			// Loops through all the buildings in the array.
-			for (var i = 0; i < openaccess.length; i++) {
-				// Creates a local variable for the current building.
-				var openacc = openaccess[i];
-				var building = openaccess[i].building;
-				delete openaccess[i].building;
-
-				// Gets the availability of the PCs available in the building.
-				var availability = (openacc.total - openacc.in_use) + ' / ' + openacc.total + ' Available';
-
-				// Checks if the building is open.
-				if (openacc.open) {
-					// Gets the percentage of available PCS in the building.
-					var percentage = (openacc.total - openacc.in_use) / openacc.total * 100;
-					// Tidies up the percentage and adds a percentage symbol.
-					var percentagePretty = Math.floor(percentage) + '%';
-
-					// Creates an HSL colour so as to keep the tone and just change the hue.
-					var colour = 'hsl(' + percentage + ', 60%, 60%)';
-
-					// Sets the width of the percentage bar to the percentage of available PCs.
-					$('#' + building.reference + ' .percent').css('width', percentagePretty);
-					// Sets the colour of the percentage bar to the HSL colour.
-					$('#' + building.reference + ' .percent').css('background', colour);
-
-					// Checks if the inline element in the paragraph exists.
-					if ($('#' + building.reference + ' p span').length === 0) {
-						// Changes the text in the availability paragraph for the building.
-						$('#' + building.reference + ' p').html(availability + '<span>&nbsp;</span>');
+		$.getJSON('http://ssd.api.port.ac.uk/v1/buildings/openaccess?access_token=bd368b12-4aed-4f5e-8797-3aa9cfe21395&callback=?', function (openaccess) {
+			// Sets the open access areas.
+			var oaa = openaccess;
+			// Loads the JSON-encoded data from the server using a GET HTTP request.
+			$.getJSON('http://ssd.api.port.ac.uk/v1/buildings?access_token=bd368b12-4aed-4f5e-8797-3aa9cfe21395&callback=?', function (buildings) {
+				// Loops through all the open access areas in the array.
+				for (var i = 0; i < openaccess.length; i++) {
+					// Loops through all the buildings in the array.
+					for (var j = 0; j < buildings.length; j++) {
+						// Checks if the open access area and building references are the same.
+						if (openaccess[i].building.reference == buildings[j].reference) {
+							// Construct array as required.
+							oaa[i]['name'] = openaccess[i].building.name;
+							oaa[i]['reference'] = openaccess[i].building.reference;
+							oaa[i]['latitude'] = buildings[j].latitude;
+							oaa[i]['longitude'] = buildings[j].longitude;
+						}
 					}
-				} else {
-					// Resets the width of the percentage bar to the percentage of available PCs.
-					$('#' + building.reference + ' .percent').css('width', '0');
-
-					// Changes the text in the availability paragraph for the building.
-					$('#' + building.reference + ' p').html('Closed');
+					// Creates a local variable for the current building.
 				}
+				// Loops through all the open access areas in the array.
+				for (var k = 0; k < oaa.length; k++) {
+					var openacc = oaa[k];
+					// Gets the availability of the PCs available in the building.
+					var availability = (openacc.total - openacc.in_use) + ' / ' + openacc.total + ' Available';
 
-				// Loads the JSON-encoded data from the server using a GET HTTP request.
-				$.getJSON(api + 'buildings/' + building.reference + access_token, function (buildings) {
+					// Checks if the building is open.
+					if (openacc.open) {
+						// Gets the percentage of available PCS in the building.
+						var percentage = (openacc.total - openacc.in_use) / openacc.total * 100;
+						// Tidies up the percentage and adds a percentage symbol.
+						var percentagePretty = Math.floor(percentage) + '%';
+
+						// Creates an HSL colour so as to keep the tone and just change the hue.
+						var colour = 'hsl(' + percentage + ', 60%, 60%)';
+
+						// Sets the width of the percentage bar to the percentage of available PCs.
+						$('#' + openacc.reference + ' .percent').css('width', percentagePretty);
+						// Sets the colour of the percentage bar to the HSL colour.
+						$('#' + openacc.reference + ' .percent').css('background', colour);
+
+						// Checks if the inline element in the paragraph exists.
+						if ($('#' + openacc.reference + ' p span').length === 0) {
+							// Changes the text in the availability paragraph for the building.
+							$('#' + openacc.reference + ' p').html(availability + '<span>&nbsp;</span>');
+						}
+					} else {
+						// Resets the width of the percentage bar to the percentage of available PCs.
+						$('#' + openacc.reference + ' .percent').css('width', '0');
+
+						// Changes the text in the availability paragraph for the building.
+						$('#' + openacc.reference + ' p').html('Closed');
+					}
+
 					// Checks that the latitude and longitude are not empty and the
 					// accuracy is at a maximum of 150.
-					if (lat != '' && lon != '' && acc <= 150) {
+					if (lat != '' && lon != '') {
 						// Runs the function to get the distance in kilometres.
-						geo(building.reference, buildings);
+						geo(openacc.reference, openacc);
 
 						// Checks if the inline element in the heading exists.
-						if ($('#' + building.reference + ' h1 span').length === 0) {
+						if ($('#' + openacc.reference + ' h1 span').length === 0) {
 							// Creates a link that navigates the user from their current location to the building.
-							$('#' + building.reference + ' h1').append('<span><a href="https://www.google.com/maps?saddr=' + lat + ',+' + lon + '&daddr=' + buildings.latitude + ',+' + buildings.longitude + '&hl=en&mra=ls&t=m&z=17" target="_blank">Map</a></span>');
+							$('#' + openacc.reference + ' h1').append('<span><a href="https://www.google.com/maps?saddr=' + lat + ',+' + lon + '&daddr=' + openacc.latitude + ',+' + openacc.longitude + '&hl=en&mra=ls&t=m&z=17" target="_blank">Map</a></span>');
 						}
 					} else {
 						// Checks if the inline element in the heading exists.
-						if ($('#' + building.reference + ' h1 span').length === 0) {
+						if ($('#' + openacc.reference + ' h1 span').length === 0) {
 							// Creates a link that pinpoints to the building's location.
-							$('#' + building.reference + ' h1').append('<span><a href="https://www.google.com/maps?q=' + buildings.latitude + ',+' + buildings.longitude + '&hl=en&t=m&z=16" target="_blank">Map</a></span>');
+							$('#' + openacc.reference + ' h1').append('<span><a href="https://www.google.com/maps?q=' + openacc.latitude + ',+' + openacc.longitude + '&hl=en&t=m&z=16" target="_blank">Map</a></span>');
 						}
-					}
-				});
-			}
-
-			// Checks that the latitude and longitude are not empty and that the
-			// accuracy is at a maximum of 150.
-			if (lat != '' && lon != '' && acc <= 150) {
-				// Sets the first building in the array as the closest by default.
-				var closest_building = buildings[0];
-
-				// Loops through all the buildings.
-				for (var j = 0; j < buildings.length; j++) {
-					// Sets the current building in a variable.
-					var building = buildings[j];
-
-					// Checks if the building is open and if it's closer to
-					// the user's location that the current closest building.
-					if (building.open && building.distance < closest_building.distance) {
-						// Sets the current building as the new closest building.
-						closest_building = building;
 					}
 				}
 
-				// Changes the text in the inline element accordingly.
-				$('#' + closest_building.reference + ' p span').html('<strong>' + $('#' + closest_building.reference + ' p span').text() + '</strong>');
-			}
+				// Checks that the latitude and longitude are not empty and that the
+				// accuracy is at a maximum of 150.
+				if (lat != '' && lon != '') {
+					// Sets the first building in the array as the closest by default.
+					var closest_building = oaa[0];
+
+					// Loops through all the buildings.
+					for (var l = 0; l < oaa.length; l++) {
+						// Sets the current building in a variable.
+						var building = oaa[l];
+
+						// Checks if the building is open and if it's closer to
+						// the user's location that the current closest building.
+						if (building.open && building.distance < closest_building.distance) {
+							// Sets the current building as the new closest building.
+							closest_building = building;
+						}
+					}
+
+					// Changes the text in the inline element accordingly.
+					$('#' + closest_building.reference + ' p span').html('<strong>' + $('#' + closest_building.reference + ' p span').text() + ' (Closest)</strong>');
+				}
+			});
 		});
 	};
 
@@ -131,7 +143,6 @@ $(document).ready(function() {
 	var error = function(err) {
 		// Clears the timeout of the fail timer.
 		clearTimeout(fail_timeout);
-		console.log(err);
 		// Runs the start function with no geolocation.
 		start('', '', '');
 	};
